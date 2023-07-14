64ad0ba2aad9559500c09aebd40693472ae7a698
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-07-14 16:19:48 +0100

    add socket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,52 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { io } from "socket.io-client";
 
-function App() {
+const URL = "http://localhost:3001/"
+const socket = io.connect(URL)
+
+const App = () => {
+
+  const [connected, setConnected] = useState(false)
+  const [message, setMessage] = useState("")
+  const [room, setRoom] = useState("")
+  
+  useEffect(() => {
+    setConnected(true)
+
+    // return () => {
+    //   socket.disconnect()
+    // }
+
+  }, [socket])
+
+  const handleSendMessage = (data) => {
+    socket.emit("fromClient", data)
+    setMessage("")
+  }
+
   return (
     <div className="App">
-      test
+      { connected ? "connected" : "not connected"}
+
+      <div>
+      <input 
+          type="text" 
+          placeholder='room'
+          onChange={(e) => setRoom(e.target.value)}
+          value={room}
+        />
+        <input 
+          type="text" 
+          placeholder='message'
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <button 
+          onClick={() => handleSendMessage({message, room})}>
+            Send
+        </button>
+      </div>
     </div>
   );
 }
