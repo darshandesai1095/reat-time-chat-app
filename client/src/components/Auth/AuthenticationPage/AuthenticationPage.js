d78932cd5c42ee91b6c680b678da6d9b2f753ec1
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-07-18 23:36:16 +0100

    authentication page

---
+++ client/src/components/Auth/AuthenticationPage/AuthenticationPage.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import './AuthenticationPage.css';
+import Login from '../Login/Login';
+import CreateAccount from '../CreateAccount/CreateAccount';
+
+const AuthenticationPage = () => {
+
+  const [login, setLogin] = useState(true) // true for login page, false for register page
+  const goToRegistrationPage = () => setLogin(false)
+  const goToLoginPage = () => setLogin(true)
+
+  return (
+    <div className="authentication-page">
+        {
+          login ? (
+
+            <Login
+              goToRegistrationPage={goToRegistrationPage}
+            />
+
+          ) : (
+
+            <CreateAccount
+              goToLoginPage={goToLoginPage}
+            />
+
+          )
+        }
+        
+        
+    </div>
+  );
+}
+
+export default AuthenticationPage;
