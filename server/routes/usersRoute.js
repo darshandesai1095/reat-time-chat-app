d3f869892ac370e7de59152a14676655cb539dd2
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-07-20 15:16:15 +0100

    add patch requests

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -15,10 +15,8 @@ router.get('/:userId', userController.getUserByMongoDBUserId)
 // Route for getting user details by Firebase UID
 router.get('/firebase/:firebaseUserId', userController.getUserByFirebaseUserId)
 
-
-
-// // Route for updating user details; username
-// router.put('/resetUsername/:userId', authorizationMiddleware, userController.updateUser)
+// Route for updating user details; username
+router.patch('/updateUsername/:userId', authorizationMiddleware, userController.updateUsername)
 
 // // Route for updating user details; email
 // router.put('/resetEmail/:userId', authorizationMiddleware, userController.updateUser)
@@ -26,13 +24,6 @@ router.get('/firebase/:firebaseUserId', userController.getUserByFirebaseUserId)
 // // Route for updating user details; pic
 // router.put('updateProfilePicture/:userId', authorizationMiddleware, userController.updateUser)
 
-
-
-// Route for adding a user to a room (assuming the room already exists)
-router.put('/:userId/rooms/:roomId', userController.addUserToRoom)
-
-// Route for removing a user to a room
-router.put('/:userId/rooms/:roomId', userController.removeUserFromRoom)
 
 // Route for deleting a user
 router.delete('/:userId', userController.deleteUser)
