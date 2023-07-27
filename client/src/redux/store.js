8f3d373724bd41e15d899fc42f4af25d6385047c
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-07-27 16:22:20 +0100

    add rtk middleware to redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
+import { userApi } from './api/auth/authApi'
 import authReducer from './features/auth/authSlice'
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+    reducer: {
+        auth: authReducer,
+        [userApi.reducerPath]: userApi.reducer
+    },
+
+    middleware: (getDefaultMiddleware) => (
+        getDefaultMiddleware().concat(userApi.middleware)
+    )
 })
+
+setupListeners(store.dispatch)
