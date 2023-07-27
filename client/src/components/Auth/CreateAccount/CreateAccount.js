52a48fa3a8cb781d9012df036a3ca6404f240cf0
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-07-27 16:22:00 +0100

    add rtk query hooks

--- client/src/components/Auth/CreateAccount/CreateAccount.js
+++ client/src/components/Auth/CreateAccount/CreateAccount.js
@@ -1,51 +1,57 @@
 import { useState, useEffect } from 'react';
-import createDatesArray from '../../../functions/createDatesArray';
-import monthsArray from '../../../functions/monthsArray';
-import createYearsArray from '../../../functions/createYearsArray';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../../firebase';
 import './CreateAccount.css';
 
-import { useSelector, useDispatch } from 'react-redux'
-import { loginRequest, loginSuccess, loginFailure, logout } from '../../../redux/features/auth/authSlice';
+import { useDispatch } from 'react-redux'
+import { loginRequest, loginSuccess, loginFailure } from '../../../redux/features/auth/authSlice';
+import { useCreateNewUserMutation } from '../../../redux/api/auth/authApi';
 
 
 const CreateAccount = ({goToLoginPage}) => {
 
-    const dispatch = useDispatch()
 
-    const datesArray = createDatesArray()
-    const yearsArray = createYearsArray()
+    const dispatch = useDispatch()
+    const [createNewAccount, result] = useCreateNewUserMutation()
 
     const [error, setError] = useState(false)
     const [email, setEmail] = useState("")
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
-    const createAccount = (event) => {
+    const handleCreateAccount = async (event) => {
         event.preventDefault()
         dispatch(loginRequest())
-
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user
-                console.log("new account created!", user)
-                dispatch(loginSuccess(user))
+    
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+            console.log("User account created:", userCredential.user.uid)
+            dispatch(loginSuccess({
+                firebaseUserId: userCredential.user.uid,
+                email: email,
+                username: username,
+            }))
+            createNewAccount({
+                firebaseUserId: userCredential.user.uid,
+                email: email,
+                username: username,
+                profilePictureUrl: "test_url_string"
             })
-            .catch((error) => {
-                setError(true)
-                dispatch(loginFailure(error.message))
-                alert("Account creation failed")
-                console.log("doesn't work", error.message)
-        })
+          
+        } catch (error) {
+            setError(true)
+            dispatch(loginFailure(error.message))
+            alert("Account creation failed")
+        }
     }
+
+    const { data, isLoading, isFetching, isError } = result
     
 
     return (
         <div className="create-account">
             <h3>Create an account</h3>
-            <form className='create-account__form' onSubmit={(e) => createAccount(e)}>
+            <form className='create-account__form' onSubmit={(e) => handleCreateAccount(e)}>
                 <div className='form__details'>
                     {
                         error ?
@@ -93,13 +99,43 @@ const CreateAccount = ({goToLoginPage}) => {
             </form>
             <p className='already-have-an-account' onClick={goToLoginPage}>Already have an account?</p>
         </div>
-    );
+    )
 }
 
 export default CreateAccount;
 
 
-// At least 7 characters in length.
+// At least 8 characters in length.
 // At least one uppercase letter (A-Z).
 // At least one lowercase letter (a-z).
 // At least one digit (0-9).
+
+
+
+// const CreateAccount = () => {
+//   const { data, error, isLoading } = useGetMessageQuery();
+
+//   // If isLoading is true, the data is currently being fetched.
+//   // You may show a loading indicator in your UI in this case.
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   // If there is an error, you can handle it or show an error message.
+//   if (error) {
+//     {console.log(error)}
+//     return <div>Error: {error.message}</div>;
+//   }
+
+//   // If data exists, it means fetching was successful, and the data is up to date.
+//   if (data) {
+//     console.log("Data:", data);
+//     // Render your component using the data
+//     return <div>{/* Your component rendering logic using data */}</div>;
+//   }
+
+//   // If none of the above conditions match, the data is not available yet or there was no data.
+//   return <div>No data available.</div>;
+// };
+
+// export default CreateAccount
