367a862382689b6a7dee03b95e9f76f413d26c66
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-07-28 15:50:08 +0100

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,18 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { io } from "socket.io-client";
-import AuthenticationPage from './components/Auth/AuthenticationPage/AuthenticationPage';
-import { auth } from './firebase';
-
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import AllChats from './components/AllChats/AllChats';
-import CurrentChat from './components/CurrentChat/CurrentChat';
-import CreateNewGroupModal from './components/AllChats/CreateNewGroupModal/CreateNewGroupModal';
-import NavBar from './components/NavBar/NavBar';
-
-import { useSelector, useDispatch } from 'react-redux'
-import { loginRequest, loginSuccess, loginFailure, logout } from './redux/features/auth/authSlice';
-
-const URL = "http://localhost:8080/"
-const socket = io.connect(URL)
+import AuthenticationPage from './components/Auth/AuthenticationPage/AuthenticationPage'
+import MainPage from './components/MainPage/MainPage';
+import { useSelector } from 'react-redux'
 
 const App = () => {
 
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
-
-  const [connected, setConnected] = useState(false)
-  const [username, setUsername] = useState(`User_${Math.floor(Math.random()*1000)}`)
-  const [room, setRoom] = useState(100)
-  const [signedIn, setSignedIn] = useState(false)
-
-  const joinRoom = (data) => {
-    socket.emit("join", data)
-  }
-
-  useEffect(() => {
-    setConnected(true)
-  }, [socket])
-
+  const isAuthenticated = useSelector(state => state.user.isAuthenticated)
 
   return (
-    <div className="App">
-      { 
-        isAuthenticated ? (
+      <div className="App">
 
-          // <>
-          //   <GroupsCol 
-          //     connected={connected}
-          //     username={username}
-          //     room={room}
-          //     setRoom={setRoom}
-          //     joinRoom={joinRoom}
-          //   />
-    
-          //   <Chat
-          //     socket={socket}
-          //     room={room}
-          //     username={username}
-          //   />
-          // </>
-          <>
-              <NavBar/>
-              <AllChats/>
-              <CurrentChat
-                socket={socket}
-                connected={connected}
-                username={username}
-                room={room}
-                setRoom={setRoom}
-                joinRoom={joinRoom}
-              />
-          </>
+          { isAuthenticated ? <MainPage/> : <AuthenticationPage/> }
 
-        ) : (
-
-          <AuthenticationPage/>
-
-        )
-      }
-
-       
-    </div>
+      </div>
   );
 }
 
