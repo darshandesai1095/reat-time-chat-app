728c45c2f33bc50620ad4987a34d3c56d7e9d72e
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-07-28 15:49:11 +0100

    register room api

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
-
-import { userApi } from './api/auth/authApi'
-import authReducer from './features/auth/authSlice'
+import { userApi } from './api/users/userApi'
+import { roomApi } from './api/rooms/roomApi'
+import userReducer from './features/users/userSlice'
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
-        [userApi.reducerPath]: userApi.reducer
+        user: userReducer,
+        [userApi.reducerPath]: userApi.reducer,
+        // room: roomReducer,
+        [roomApi.reducerPath]: roomApi.reducer
     },
 
     middleware: (getDefaultMiddleware) => (
-        getDefaultMiddleware().concat(userApi.middleware)
+        getDefaultMiddleware().concat(
+          userApi.middleware, 
+          roomApi.middleware
+        )
     )
 })
 
