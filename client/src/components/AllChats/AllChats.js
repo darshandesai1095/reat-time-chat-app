fe91f46fe371b924bdb9decf509075b3fea6d668
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-07-31 22:48:56 +0100

    get rooms data request

--- client/src/components/AllChats/AllChats.js
+++ client/src/components/AllChats/AllChats.js
@@ -1,10 +1,32 @@
 import './AllChats.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AllChatsBody from './AllChatsBody/AllChatsBody';
 import AllChatsHeader from './AllChatsHeader/AllChatsHeader';
 import CreateNewGroupModal from './CreateNewGroupModal/CreateNewGroupModal';
+import { useSelector, useDispatch } from 'react-redux'
+import { getRoomsByFirebaseUserId } from '../../redux/features/rooms/roomSlice';
+
+
+
 
 const AllChats = ({}) => {
+
+    const dispatch = useDispatch()
+    const firebaseUserId = useSelector(state => state.user.firebaseUserId)
+    console.log("all chats: ", firebaseUserId)
+    useEffect(() => {
+
+        if (firebaseUserId) {
+            try {
+                dispatch(getRoomsByFirebaseUserId( firebaseUserId ))
+            } catch (error) {
+                console.log(error)
+            }
+        } else {
+            console.log("no id") // send to error/loading page
+        }
+
+    }, [])
 
     const [showCreateGroupPopup, setShowCreateGroupPopup] = useState(false)
 
