8ec27c7cbda83ea0f23b6dee18a4592f8b5b5b86
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-02 17:56:10 +0100

    add onclick to change active room

--- client/src/components/AllChats/ChatGroup/ChatGroup.js
+++ client/src/components/AllChats/ChatGroup/ChatGroup.js
@@ -1,36 +1,45 @@
 import './ChatGroup.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeCurrentActiveRoom } from '../../../redux/features/rooms/roomSlice';
 
-const ChatGroup = ({ connected, roomName, room, setRoom, joinRoom }) => {
+const ChatGroup = ({ connected, roomName, roomId, setRoom, joinRoom, active }) => {
 
+    const dispatch = useDispatch()
+    const activateRoom = () => {
+        dispatch(changeCurrentActiveRoom(roomId))
+    }
 
-  return (
-    <div className="chat-group" >
+    return (
+        <div 
+            className={`chat-group ${active ? "active-chat" : null}`} 
+            onClick={activateRoom}
+        >
 
-            <img src="man.png" className='chat-group__avatar'/>
+                <img src="man.png" className='chat-group__avatar'/>
 
-            <div className='chat-group__message'>
-                <div className='message__sender'>
-                    <p>{roomName}</p>
+                <div className='chat-group__message'>
+                    <div className='message__sender'>
+                        <p>{roomName}</p>
+                    </div>
+                    <div className='message__preview'>
+                        <p>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt 
+                            ut labore et dolore magna aliqua..
+                        </p>
+                    </div>
+        
                 </div>
-                <div className='message__preview'>
-                    <p>Lorem ipsum dolor sit amet, 
-                        consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt 
-                        ut labore et dolore magna aliqua..
-                    </p>
+
+                <div className='chat-group__meta-data'>
+                    <div className='meta-data__time'>
+                        <p>21:30</p>
+                    </div>
                 </div>
+        
     
-            </div>
-
-            <div className='chat-group__meta-data'>
-                <div className='meta-data__time'>
-                    <p>21:30</p>
-                </div>
-            </div>
-     
-  
-    </div>
-  );
+        </div>
+    );
 }
 
 export default ChatGroup
