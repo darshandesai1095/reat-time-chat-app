995edb3a78e98076548a90c9593f14ebb5f7690c
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-03 16:07:33 +0100

    add loading page

--- client/src/components/CurrentChat/DeleteGroupModal/DeleteGroup.js
+++ client/src/components/CurrentChat/DeleteGroupModal/DeleteGroup.js
@@ -1,41 +1,58 @@
-import { useState } from 'react';
 import './DeleteGroup.css';
-import InputField from '../../InputField/InputField';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { useSelector, useDispatch } from 'react-redux';
+import deleteRoomAndSyncData from '../../../functions/rooms/deleteRoomAndSyncData';
+import LoadingModal from '../../AllChats/LoadingModal/LoadingModal';
 
-const DeleteGroup = ({deleteGroupModalVisible, setDeleteGroupModalVisible}) => {
+const DeleteGroup = ({deleteGroupModalVisible, setDeleteGroupModalVisible, activeRoomId, mongoDbUserId}) => {
 
-    const [groupName, setGroupName] = useState('')
+    const isLoading  = useSelector(state => state.rooms.loading)
+    const dispatch = useDispatch()
 
     const closePopup = () => {
         setDeleteGroupModalVisible(false)
-        setGroupName('')
     }
 
-    const deleteGroup = () => {
+    const deleteGroup = async () => {
+        try {
+            await deleteRoomAndSyncData(dispatch, activeRoomId, mongoDbUserId)
+        } catch (error) {
+            alert("Error updating group name!")
+        }
         setDeleteGroupModalVisible(false)
-        setGroupName('')
     }
 
     return (
-        <div className={`modal-background ${deleteGroupModalVisible ? null : "hide-page"}`}>
-            <div className="create-new-group">
-                <h3>Delete Group</h3>
-                <div className='close-icon'>
-                    <CloseRoundedIcon
-                        onClick={closePopup}
-                    />
-                </div>
 
-                <div className='create-new-group__details group-name'>
-                    <p className='delete-group__message'>
-                        PRESS DELETE TO CONFIRM DELETION. <br/> THIS ACTION CANNOT BE UNDONE.
-                    </p>
-                </div>
+        <>
+        {
+            isLoading ?
 
-                <button className='close-popup-button delete-group__button' onClick={deleteGroup} style={{marginBottom: "5px"}}>Delete Forever</button>
+            <LoadingModal/>
+
+            :
+
+            <div className={`modal-background ${deleteGroupModalVisible ? null : "hide-page"}`}>
+                <div className="create-new-group">
+                    <h3>Delete Group</h3>
+                    <div className='close-icon'>
+                        <CloseRoundedIcon
+                            onClick={closePopup}
+                        />
+                    </div>
+
+                    <div className='create-new-group__details group-name'>
+                        <p className='delete-group__message'>
+                            Press delete to confirm deletion. <br/> This action cannot be undone.
+                        </p>
+                    </div>
+
+                    <button className='close-popup-button delete-group__button' onClick={deleteGroup} style={{marginBottom: "5px"}}>Delete</button>
+                </div>
             </div>
-        </div>
+
+        }
+        </>
     );
 }
 
