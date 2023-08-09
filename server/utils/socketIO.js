a07cd67f30ebd7f8e12b1ecb720a9d1a8b673eaf
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-09 18:05:02 +0100

    remove add message id from server + add to client

--- server/utils/socketIO.js
+++ server/utils/socketIO.js
@@ -1,5 +1,4 @@
 const { client } = require('../config/connectToRedis')
-const { v4: uuidv4 } = require('uuid')
 
 
 const socket = (IO) => {
@@ -26,30 +25,38 @@ const socket = (IO) => {
 
             // messageData = { roomId, senderId, username, messageContent, dateCreated }
 
-            // set meta data for message
-            const messageId = uuidv4()
-
             // get previous messages from redis and append new message
             try {
                 const jsonMessageLog = await client.hGet('chatLogs', messageData.roomId)
                 const messageLog = jsonMessageLog ? JSON.parse(jsonMessageLog) : []
 
+                // const currentMessage = {
+                //     messageId: messageId, // ✓
+                //     roomId: messageData.roomId, // ✗
+                //     senderId: messageData.senderId, // ✓
+                //     username: messageData.username, // ✓
+                //     messageContent: messageData.messageContent, // ✓
+                //     dateCreated: messageData.dateCreated, // ✓
+                //     status: "sent" // ✗
+                // }
+
                 const currentMessage = {
-                    messageId: messageId,
                     roomId: messageData.roomId,
-                    senderId: messageData.senderId,
-                    username: messageData.username,
-                    messageContent: messageData.messageContent,
-                    dateCreated: messageData.dateCreated,
-                    status: "sent"
+                    message: {
+                        messageId: messageData.message.messageId, // ✓ 
+                        senderId: messageData.message.senderId, // ✓
+                        username: messageData.message.username, // ✓
+                        messageContent: messageData.message.messageContent, // ✓
+                        dateCreated: messageData.message.dateCreated, // ✓
+                    }
                 }
 
                 // save to redis
-                await client.HSET('chatLogs', messageData.roomId, JSON.stringify([...messageLog, currentMessage]))
+                await client.HSET('chatLogs', messageData.roomId, JSON.stringify([...messageLog, currentMessage.message]))
 
                 // callback sent to sender
                 const response = {
-                    success: true,
+                    success: true, // client: if success===true -> deliveryStatus->"sent"
                     data: currentMessage
                 }
                 acknowledgment(response)
@@ -57,7 +64,7 @@ const socket = (IO) => {
                 // send message to rest of room 
                 // messageData = { roomId, { messageId, senderId, messageContent, dateCreated} }
                 // senderId: mapped to -> userId, firebaseUserId, email, username
-                socket.to(messageData.roomId).emit('message', messageData)
+                socket.to(messageData.roomId).emit('message', currentMessage)
 
             } catch (error) {
                 console.log("Redis error", error)
