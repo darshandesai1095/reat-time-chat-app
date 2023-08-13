c496c347372e5f93f0395009dfface665ce70caf
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-13 16:12:15 +0100

    add delivery status selector

--- client/src/components/CurrentChat/MessageBubble/MessageBubble.js
+++ client/src/components/CurrentChat/MessageBubble/MessageBubble.js
@@ -2,9 +2,9 @@ import { useSelector } from 'react-redux';
 import './MessageBubble.css';
 
 
-const MessageBubble = ({prevMessageSender, messageObj}) => {
+const MessageBubble = ({ prevMessageSender, messageObj }) => {
 
-    const { username, senderId, messageContent, dateCreated } = messageObj
+    const { username, senderId, messageContent, dateCreated, messageId, deliveryStatus } = messageObj
     const clientUsername = useSelector(state => state.user.username)
     const clientsenderId = useSelector(state => state.user.senderId)
 
@@ -19,8 +19,7 @@ const MessageBubble = ({prevMessageSender, messageObj}) => {
                 <p className='main__message'>{messageContent}</p>
                 <div className='main__meta-data'>
                     <p className='meta-data__time'>{dateCreated}</p>
-                    {/* <AddCircleOutlineRoundedIcon style={{transform:"scale(0.5)", margin:"0px", padding:"0px"}}/> */}
-                    <p className='meta-data__add-to-list'></p>
+                    <p>{deliveryStatus}</p>
                 </div>
             </div>
 
