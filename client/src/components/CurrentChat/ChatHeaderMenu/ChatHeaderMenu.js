59583b934f6ff52d568b27f414c6707569902798
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-16 20:28:55 +0100

    redesign menu

--- client/src/components/CurrentChat/ChatHeaderMenu/ChatHeaderMenu.js
+++ client/src/components/CurrentChat/ChatHeaderMenu/ChatHeaderMenu.js
@@ -7,20 +7,29 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import DeleteIcon from '@mui/icons-material/Delete';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import GroupRemoveIcon from '@mui/icons-material/GroupRemove';
+import PendingRoundedIcon from '@mui/icons-material/PendingRounded';
+
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import GroupAddRoundedIcon from '@mui/icons-material/GroupAddRounded';
+import GroupRemoveRoundedIcon from '@mui/icons-material/GroupRemoveRounded';
+import DoorFrontRoundedIcon from '@mui/icons-material/DoorFrontRounded';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+
+
 
 
 const ChatHeaderMenu = ({ setUpdateGroupNamePopupVisible, setUpdateAddUsersModalVisible, setRemoveUsersModalVisible, setDeleteGroupModalVisible }) => {
 
 
-    const [menuIconsVisible, setMenuIconsVisible] = useState(false)
-    const toggleMenu = () => {
-        setMenuIconsVisible(prev => !prev)
+    const [dropdownVisible, setDropdownVisible] = useState(false)
+    const toggleDropdown = () => {
+        setDropdownVisible(prev => !prev)
     }
 
     return (
 
         <div className='chat-header-menu'>
-            <CreateRoundedIcon
+            {/* <CreateRoundedIcon
                 className={menuIconsVisible ? "visible" : "hidden"}
                 onClick={() => setUpdateGroupNamePopupVisible(true)}
             />
@@ -35,15 +44,43 @@ const ChatHeaderMenu = ({ setUpdateGroupNamePopupVisible, setUpdateAddUsersModal
             <DeleteIcon
                 className={menuIconsVisible ? "visible" : "hidden"}            
                 onClick={() => setDeleteGroupModalVisible(true)}
-            />
+            /> */}
 
-            <MoreVertIcon
-                className={menuIconsVisible ? "rot45deg x close-menu" : "rot0deg x"}    
-            />
-            <MoreVertIcon
-                className={menuIconsVisible ? "rot-45deg close-menu" : "rot0deg"}    
+            {/* <PendingRoundedIcon 
+                className={menuIconsVisible ? "visible" : "hidden"}
                 onClick={toggleMenu}
-            />
+            /> */}
+
+            <div className='icon-padding'>
+                <PendingRoundedIcon
+                    className={dropdownVisible ? "rot45deg" : "rot0deg"}
+                    onClick={toggleDropdown}
+                />
+            </div>
+
+            <div className={`menu__dropdown ${dropdownVisible ? "show" : "hide"}`}>
+                <div className='dropdown__item'>
+                    <EditRoundedIcon/>
+                    <p>Rename Group</p>
+                </div>
+                <div className='dropdown__item'>
+                    <GroupAddRoundedIcon/>
+                    <p>Add Users</p>
+                </div>
+                <div className='dropdown__item'>
+                    <GroupRemoveRoundedIcon/>
+                    <p>Remove Users</p>
+                </div>
+                <div className='dropdown__item'>
+                    <DoorFrontRoundedIcon/>
+                    <p>Leave Group</p>
+                </div>
+                <div className='dropdown__item'>
+                    <DeleteRoundedIcon/>
+                    <p>Delete Group</p>
+                </div>
+            </div>
+
         </div>
 
     )
