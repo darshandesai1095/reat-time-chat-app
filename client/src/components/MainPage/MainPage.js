0ba9ec04c16b3ededbcab72a5bf0551a6a91c35c
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-16 20:29:27 +0100

    add local storage functions

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -8,6 +8,8 @@ import { getUserByFirebaseUserId } from '../../redux/features/users/userSlice';
 import { getChatLogsByFirebaseUserId } from '../../redux/features/chatLogs/chatLogSlice';
 import { socket, socketIoListenForMessage, socketIoHeartbeat } from '../../redux/socket/socketIO';
 import { socketIoJoinRooms } from '../../redux/features/chatLogs/chatLogSlice';
+import { getLastActiveFromLocalStorage, setLastActiveInLocalStorage } from '../../functions/misc/localStorage';
+import { syncActivityLogWithLocalStorage, updateActivityLog } from '../../redux/features/activityLogs/activityLogSlice';
 
 const MainPage = () => {
 
@@ -19,6 +21,7 @@ const MainPage = () => {
     }, [socket])
 
     const firebaseUserId = useSelector(state => state.user.firebaseUserId)
+    const userId = useSelector(state => state.user.mongoDbUserId)
     const userError = useSelector(state => state.user.userError)
     const roomsArray = useSelector(state => state.user.rooms)
 
@@ -55,6 +58,36 @@ const MainPage = () => {
         }
     }
     joinRoomsSocketIo()
+
+    // get activity log from local storage and save to redux store
+    const activityLog = useSelector(state => state.activityLog.lastActive)
+    useEffect(() => {
+        if (userId !== null) {
+            const activityLog = getLastActiveFromLocalStorage(userId)
+            console.log("main page activity log", activityLog)
+            dispatch(syncActivityLogWithLocalStorage(activityLog))
+        }
+
+        console.log("redux store log", activityLog)
+        
+
+    }, [userId])
+
+    // // save activity log from redux store to local storage
+    // useEffect(() => {
+    //     const handleBeforeUnload = () => {
+    //         if (userId) {
+    //             setLastActiveInLocalStorage(userId, activityLog)
+    //         }
+    //     }
+
+    //     window.addEventListener('beforeunload', handleBeforeUnload)
+
+    //     return () => {
+    //         // Remove the event listener when the component unmounts
+    //         window.removeEventListener('beforeunload', handleBeforeUnload)
+    //     }
+    // }, [])
 
 
     return (
