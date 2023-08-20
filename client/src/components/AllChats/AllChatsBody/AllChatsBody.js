81a731fe8d227fbb8c270bf591ca527bf2b77631
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-20 19:13:02 +0100

    display last message time

--- client/src/components/AllChats/AllChatsBody/AllChatsBody.js
+++ client/src/components/AllChats/AllChatsBody/AllChatsBody.js
@@ -3,24 +3,48 @@ import ChatGroup from "../ChatGroup/ChatGroup"
 import { useDispatch, useSelector } from 'react-redux';
 import { socketIoListenForGlobalAlert } from '../../../redux/socket/socketIO';
 import { useEffect, useState } from 'react';
+import convertToUnixTimestamp from '../../../functions/misc/convertToUnixTimestamp';
 
 
-const AllChatsBody = () => {
+const AllChatsBody = ({ search }) => {
 
     const userId = useSelector(state => state.user.mongoDbUserId)
     const userRooms = useSelector(state => state.user.rooms)
+    const roomsActivityLog = useSelector(state => state.activityLog?.lastActive)
+    const chatLogsData = useSelector(state => state.chatLogs.chatLogData)
     const dispatch = useDispatch()
 
     const [activityLog, setActivityLog] = useState(null)
 
     useEffect(() => {
-        socketIoListenForGlobalAlert(dispatch, userId, userRooms)
+        socketIoListenForGlobalAlert(dispatch, userId, userRooms, roomsActivityLog)
     }, [userId, userRooms])
 
     const currentActiveRoomId = useSelector(state => state.rooms.currentActiveRoomId)
 
     const roomsData = useSelector(state => state.rooms.roomsData)
-    const chatRooms = roomsData?.map(room => {
+
+    // sort roomsData by last active
+    // >  get last active from local storage?
+    // const roomsDataSortedByLastActive = roomsData.sort()
+    const date = Date.now()
+    let shallowCopyChatLogsData = chatLogsData ? [...chatLogsData] : []
+    shallowCopyChatLogsData?.sort((room1, room2) => {
+        const room1MessagesArray = room1.messagesArray
+        const room1TotalMessages = room1MessagesArray.length
+        const lastMessageTime1 = room1TotalMessages > 0 ? room1MessagesArray[room1TotalMessages-1].dateCreated : date-1
+        const lastMessageTime1Unix = convertToUnixTimestamp(lastMessageTime1)
+
+        const room2MessagesArray = room2.messagesArray
+        const room2TotalMessages = room2MessagesArray.length
+        const lastMessageTime2 = room2TotalMessages > 0 ? room2MessagesArray[room2TotalMessages-1].dateCreated : date-1
+        const lastMessageTime2Unix = convertToUnixTimestamp(lastMessageTime2)
+
+        return (lastMessageTime1Unix - lastMessageTime2Unix)
+    })
+
+
+    const chatRooms = shallowCopyChatLogsData?.map(room => {
       const roomId = room.roomId
       return (
           <ChatGroup
@@ -30,17 +54,20 @@ const AllChatsBody = () => {
             active={ currentActiveRoomId === room.roomId ? true : false }
             activityLog={ activityLog ? activityLog : {} }
             setActivityLog = {setActivityLog}
+            search={search}
           />
       )
     })
+
+    console.log("roomsData", roomsData)
 
 
     return (
 
       <div className="all-chats-body">
-        {chatRooms?.reverse()}
+        { chatRooms?.reverse() }
       </div>
     )
 }
 
-export default AllChatsBody;
+export default AllChatsBody
