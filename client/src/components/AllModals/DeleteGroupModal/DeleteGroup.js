2d9d792e786202a5d00507c455cea10f933e19e8
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-24 15:34:55 +0100

    delete group

---
+++ client/src/components/AllModals/DeleteGroupModal/DeleteGroup.js
@@ -0,0 +1,66 @@
+import './DeleteGroup.css';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { useSelector, useDispatch } from 'react-redux';
+import deleteRoomAndSyncData from '../../../functions/rooms/deleteRoomAndSyncData';
+import LoadingModal from '../LoadingModal/LoadingModal';
+import { toggleShowDeleteGroupModal } from '../../../redux/features/modals/modalSlice';
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
+import { setLoading } from '../../../redux/features/rooms/roomSlice';
+
+
+const DeleteGroup = ({activeRoomId, mongoDbUserId}) => {
+
+    const isLoading  = useSelector(state => state.rooms.loading)
+    const showDeleteGroupModal = useSelector(state => state.modals.showDeleteGroupModal)
+    const dispatch = useDispatch()
+
+    const closePopup = () => {
+        dispatch(toggleShowDeleteGroupModal())
+    }
+
+    const deleteGroup = async () => {
+        dispatch(toggleShowDeleteGroupModal())
+        dispatch(setLoading())
+        try {
+            await deleteRoomAndSyncData(dispatch, activeRoomId, mongoDbUserId)
+        } catch (error) {
+            alert("Error deleting group!")
+        }
+
+    }
+
+    return (
+
+        <>
+        {
+            isLoading ?
+
+            <LoadingModal/>
+
+            :
+
+            <div className={`modal-background ${showDeleteGroupModal ? null : "hide-page"}`}>
+                <div className="create-new-group">
+                    <h3>Delete Group</h3>
+                    <div className='close-icon'>
+                        <CancelRoundedIcon
+                            onClick={closePopup}
+                        />
+                    </div>
+
+                    <div className='create-new-group__details group-name'>
+                        <p className='delete-group__message'>
+                            You are about to delete this group. <br/> This action cannot be undone.
+                        </p>
+                    </div>
+
+                    <button className='close-popup-button delete-group__button' onClick={deleteGroup} style={{marginBottom: "5px"}}>Delete</button>
+                </div>
+            </div>
+
+        }
+        </>
+    );
+}
+
+export default DeleteGroup;
