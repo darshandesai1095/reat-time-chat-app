c0a3cfef85aa6b1554689524924b7d7656d95365
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-24 15:39:25 +0100

    add modal component

---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,71 @@
+import AddMoreUsersModal from '../AllModals/AddMoreUsersModal/AddMoreUsersModal';
+import CreateNewGroupModal from '../AllModals/CreateNewGroupModal/CreateNewGroupModal';
+import DeleteGroup from '../AllModals/DeleteGroupModal/DeleteGroup';
+import LeaveGroupModal from '../AllModals/LeaveGroupModal/LeaveGroupModal';
+import RemoveUsersModal from '../AllModals/RemoveUsersModal/RemoveUsersModal';
+import SettingsModal from '../AllModals/SettingsModal/SettingsModal';
+import UpdateGroupNameModal from '../AllModals/UpdateGroupNameModal/UpdateGroupNameModal';
+import './Modal.css';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes, Link, Outlet } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import ChangeGroupChatIconModal from '../AllModals/ChangeGroupChatIconModal/ChangeGroupChatIconModal';
+
+
+const Modal = () => {
+
+    const activeRoomId = useSelector(state => state.rooms.currentActiveRoomId)
+    const roomsData = useSelector(state => state.rooms.roomsData)
+    const currentActiveRoomIndex = roomsData?.findIndex(room => room.roomId == activeRoomId)
+    const mongoDbUserId = useSelector(state => state.user.mongoDbUserId)
+    const showChangeGroupIconModal = useSelector(state => state.modals.showChangeGroupIconModal)
+
+
+    return (
+        <>
+            <CreateNewGroupModal/>
+
+            {
+                showChangeGroupIconModal ?
+                <ChangeGroupChatIconModal
+                    modalTitle="Select Group Icon"
+                    buttonDescription="Update Icon"
+                    activeRoomId={activeRoomId}
+                /> : null
+
+            }
+
+
+            <UpdateGroupNameModal
+                activeRoomId={activeRoomId}
+                mongoDbUserId={mongoDbUserId || null}
+                roomName={roomsData ? roomsData[currentActiveRoomIndex]?.roomName : null}
+            />
+            <AddMoreUsersModal
+                activeRoomId={activeRoomId}
+                mongoDbUserId={mongoDbUserId || null}
+            />
+            <RemoveUsersModal
+                activeRoomId={activeRoomId}
+                mongoDbUserId={mongoDbUserId || null}
+                usersList={roomsData ? roomsData[currentActiveRoomIndex]?.roomUsers : null}
+            />
+
+            <LeaveGroupModal
+                activeRoomId={activeRoomId}
+                mongoDbUserId={mongoDbUserId || null}           
+            />
+
+            <DeleteGroup
+                activeRoomId={activeRoomId}
+                mongoDbUserId={mongoDbUserId || null}
+            />
+
+            <SettingsModal/>
+
+            
+        </>
+    )
+}
+
+export default Modal
