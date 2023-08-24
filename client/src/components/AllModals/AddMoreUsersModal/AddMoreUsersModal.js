280a2d19c8bd9f743703dfdbd606d7d35bee31cd
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-08-24 15:34:11 +0100

    add more users modal

---
+++ client/src/components/AllModals/AddMoreUsersModal/AddMoreUsersModal.js
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import './AddMoreUsersModal.css';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import InputField from '../../InputField/InputField';
+import addUsersToRoomAndSyncData from '../../../functions/rooms/addUsersToRoomAndSyncData';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleShowAddMoreUsersModal } from '../../../redux/features/modals/modalSlice';
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
+
+
+
+const AddMoreUsersModal = ({mongoDbUserId, activeRoomId}) => {
+
+    const dispatch = useDispatch()
+    const showAddMoreUsersModal = useSelector(state => state.modals.showAddMoreUsersModal)
+
+    const [emails, setEmails] = useState([''])
+    const handleAddMore = () => {
+        setEmails([...emails, ''])
+    }
+
+    const closePopup = () => {
+        dispatch(toggleShowAddMoreUsersModal())
+        setEmails([''])
+    }
+
+    const handleUpdateUsers = async () => {
+        dispatch(toggleShowAddMoreUsersModal())
+        try {
+            // if user not in removedrooms list, do below, else
+            await addUsersToRoomAndSyncData(dispatch, activeRoomId, mongoDbUserId, emails)
+        } catch (error) {
+            alert("Error adding users!")
+        }
+        setEmails([''])
+    }
+
+    return (
+        <div className={`modal-background ${showAddMoreUsersModal ? null : "hide-page"}`}>
+            <div className="create-new-group">
+                <h3>Add Users</h3>
+                <div className='close-icon'>
+                    <CancelRoundedIcon
+                        onClick={closePopup}
+                    />
+                </div>
+
+                <div className='create-new-group__details'>
+                    {/* <p>USER EMAIL</p> */}
+                    {emails.map( (email, i) => {
+                        return (
+                            <div className='email-input-field' key={i}>
+
+                                <InputField
+                                    // key={i}
+                                    value={emails[i]}
+                                    setValue={(newValue) => {
+                                        const updatedEmails = [...emails]
+                                        updatedEmails[i] = newValue
+                                        setEmails(updatedEmails)
+                                    }}
+                                    required={false}
+                                    placeholder={"Enter user email"}
+                                />
+
+                            </div>
+      
+                        )
+                    })}
+                </div>
+
+                <p className='add-more' onClick={handleAddMore}>Add more</p>
+
+                <button className='close-popup-button' onClick={handleUpdateUsers}>Add To Group</button>
+            </div>
+        </div>
+    );
+}
+
+export default AddMoreUsersModal;
