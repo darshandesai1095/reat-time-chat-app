a4690ac373817f050cf52f5e38afddebc012b5ce
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-09-01 21:35:59 +0100

    add in user error log notif

--- client/src/components/NavBar/NotificationsMenu/NotificationsMenu.js
+++ client/src/components/NavBar/NotificationsMenu/NotificationsMenu.js
@@ -1,13 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import NotificationItem from '../NotificationItem/NotificationItem';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import './NotificationsMenu.css';
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+import { useState } from 'react';
+import { clearNotifications } from '../../../redux/features/globalAlerts/globalAlertSlice';
+import { clearUserErrorLog } from '../../../redux/features/users/userSlice';
 
 
 const NotificationsMenu = ({ setNotificationsMenuVisible }) => {
 
     const alertLog = useSelector(state => state.globalAlerts.alertLog)
-    const notifications = alertLog.map( (item, i) => {
+    const userErrorLog = useSelector(state => state.user.userErrorLog)
+    console.log("userErrorLog", userErrorLog)
+
+    const notifications = [...alertLog, ...userErrorLog].map( (item, i) => {
         return (
             <NotificationItem
                 key={i}
@@ -15,6 +22,21 @@ const NotificationsMenu = ({ setNotificationsMenuVisible }) => {
             />
         )
     })
+
+    const [expandMenu, setExpandMenu] = useState(false)
+    const userId = useSelector(state => state.user.mongoDbUserId)
+    const dispatch = useDispatch()
+    const handleClearNotifications = async () => {
+        try {
+            dispatch(clearUserErrorLog())
+            await Promise.resolve(dispatch(clearNotifications({userId})))
+        } catch (error) {
+            console.log("clear notifications error", error)
+        }
+
+    }
+
+    const notificationsStatus = useSelector(state => state.globalAlerts.status)
 
     return (
         <div className="notifications-menu">
@@ -24,15 +46,47 @@ const NotificationsMenu = ({ setNotificationsMenuVisible }) => {
                     onClick={() => setNotificationsMenuVisible(false)}
                 />
             </div>
-            {   alertLog?.length > 0 ?
-                notifications
-                :
-                <div className="notification-item">
-                    <div className='notification-item__header'>
-                        <p> { "New notifications will appear here" } </p>
+            <div className={`notifications-menu__body ${expandMenu ? "expand" : "contract"}`}>
+                {   [...alertLog, ...userErrorLog]?.length > 0 ?
+                    notifications?.reverse()
+                    :
+                    <div className="notification-item">
+                        <div className='notification-item__header'>
+                            <p> { "New notifications will appear here" } </p>
+                        </div>
                     </div>
+                }
+            </div>
+
+            {
+                [...alertLog, ...userErrorLog]?.length > 0 ?
+
+                <div className='alerts__expand'>
+                    <ExpandMoreRoundedIcon
+                        onClick={() => setExpandMenu(prev => !prev)}
+                        style={{
+                            transform: `${expandMenu ? "rotate(180deg)" : "rotate(0deg)"}`,
+                            transition: "0.1s"
+                        }}
+                    />
+                    {
+                        notificationsStatus === "Clearing Notifications" ?
+                        <p className='clearing-notifications'>
+                            Clearing Notifications...
+                        </p>
+                        :
+                        <p onClick={handleClearNotifications} className='clear-notifications'>
+                            Clear All
+                        </p>
+                    }
+        
                 </div>
+
+                :
+
+                null
             }
+           
         </div>
     )
 }
