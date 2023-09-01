13ab8042592c3b64161410cd7610a642a00b0563
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-09-01 21:35:02 +0100

    add error message alerts

--- client/src/components/NavBar/NotificationItem/NotificationItem.js
+++ client/src/components/NavBar/NotificationItem/NotificationItem.js
@@ -1,22 +1,32 @@
 import formatDate from '../../../functions/misc/formatDate';
 import './NotificationItem.css';
+import ErrorOutlineRoundedIcon from '@mui/icons-material/ErrorOutlineRounded';
 
-
-const NotificationItem = ({ alert }) => {
+const NotificationItem = ({ alert }) => { // alt format { alertTitle, alertTime, alertMessage }
 
     return (
         <div className="notification-item">
             <div className='notification-item__header'>
-                <h4> {alert.alertTitle} </h4>
+                <div className='notification-item__header-title'> 
+                    <h4>
+                        {alert.alertTitle} 
+                    </h4>
+                    { alert.isError ? <ErrorOutlineRoundedIcon/> : null}
+                </div>
                 <p> {formatDate(alert.alertTime, "difference")} </p>
             </div>
             <div className='notification-item__body'>
                 <p>
-                    { alert.alertTitle === "Group Deleted" ? `${alert.alertInfo.roomName} was deleted by ${alert.alertInfo.deletedBy}` : null }
+                    { alert.alertTitle === "Group Deleted" ? `${alert.alertInfo.deletedBy} deleted ${alert.alertInfo.roomName}` : null }
                     { alert.alertTitle === "Group Name Updated" ? `${alert.alertInfo.originalRoomName} was changed to ${alert.alertInfo.roomName} by ${alert.alertInfo.updatedByUsername}` : null }
                     { alert.alertTitle === "User Added To Group" ? `${alert.alertInfo.updatedByUsername} added you to ${alert.alertInfo.roomName}` : null }
                     { alert.alertTitle === "User Removed From Group" ? `${alert.alertInfo.updatedByUsername} removed you from ${alert.alertInfo.roomName}` : null }
                     { alert.alertTitle === "New Group Created" ? `${alert.alertInfo.createdByUsername} created ${alert.alertInfo.roomName}` : null }
+                    
+                    { alert.alertTitle === "Error Deleting Group" ? `An error occured whilst trying to delete ${alert.roomName}` : null }
+                    
+                    { alert.alertTitle === "Error Deleting Account" ? `${alert.alertMessage}` : null }
+
                 </p>
             </div>
         </div>
