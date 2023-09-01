0a254275a38c99511b5865630f35853917a939a2
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-09-01 21:48:04 +0100

    check online status

--- client/src/redux/socket/socketIO.js
+++ client/src/redux/socket/socketIO.js
@@ -1,11 +1,12 @@
 import { io } from "socket.io-client";
-import { getNewRoomData } from "../features/rooms/roomSlice";
+import { getNewRoomData, updateRoomNameReducer } from "../features/rooms/roomSlice";
 import { addRoomToUserRoomsList, socketIoLeaveRoom, removeRoomFromUserSlice } from "../features/users/userSlice";
 import { removeRoomFromRoomSlice } from "../features/rooms/roomSlice";
 import { pushMessageToChatLog, getNewChatLogData, socketIoJoinRooms, removeRoomFromChatLogSlice } from "../features/chatLogs/chatLogSlice";
 import { updateActivityLog } from "../features/activityLogs/activityLogSlice";
 import { setLastActiveInLocalStorage } from "../../functions/misc/localStorage";
 import { updateAlertLog } from "../features/globalAlerts/globalAlertSlice";
+import { updateOnlineUsersList } from "../features/users/onlineUsersSlice";
 
 
 // connect to socket.io
@@ -25,21 +26,29 @@ export const socketIoListenForMessage = (dispatch) => {
     })
 }
 
-export const socketIoHeartbeat = () => {
-    socket.on('ping', () => {
-        socket.emit('pong', {beat: 1})
+export const checkOnlineStatus = (dispatch) => {
+    socket.on('ping', (data) => {
+        socket.emit('pong')
+        dispatch(updateOnlineUsersList(data))
     })
 }
 
-export const socketIoListenForGlobalAlert = (dispatch, userId, userRooms, activityLog) => {
+export const socketIoListenForGlobalAlert = (dispatch, userId, userRooms, activityLog, transmissionIDs, setTransmissionIDs) => {
     socket.on('globalAlert', (data) => {
+        console.log("global alert data", transmissionIDs, data)
+        if ( [data.transmissionID] in transmissionIDs ) {
+            return
+        } else {
+            // add to set
+            setTransmissionIDs( { ...transmissionIDs, [data.transmissionID]: true } )
+        }
 
         if (data.eventType === 'newRoomCreated') {
             const newRoomId = data.eventData.roomId
             // if (data.eventData.users.includes(userId) ) {
           
             // }
-            if (data.eventData.users.includes(userId) && (data.eventData.createdBy !== userId)) {
+            if (data.eventData.users.includes(userId)) {
                 dispatch(socketIoJoinRooms([newRoomId]))
                 
                 dispatch(addRoomToUserRoomsList(newRoomId))
@@ -63,14 +72,18 @@ export const socketIoListenForGlobalAlert = (dispatch, userId, userRooms, activi
                     [newRoomId]: date
                 })
 
-                dispatch(updateAlertLog({
-                    alertTitle: "New Group Created",
-                    alertTime: date,
-                    alertInfo: {
-                        roomName: data.eventData.roomName,
-                        createdByUsername: data.eventData.createdByUsername
-                    }
-                }))
+                if (data.eventData.createdBy !== userId) {
+                    dispatch(updateAlertLog({
+                        transmissionId: data.transmissionID,
+                        alertTitle: "New Group Created",
+                        alertTime: date,
+                        alertInfo: {
+                            roomName: data.eventData.roomName,
+                            createdByUsername: data.eventData.createdByUsername
+                        }
+                    }))
+                }
+
             }
 
         }
@@ -94,12 +107,15 @@ export const socketIoListenForGlobalAlert = (dispatch, userId, userRooms, activi
                     timestamp: date
                 }))
 
+                // need to fetch roomchatlog
+
                 setLastActiveInLocalStorage(userId, {
                     ...activityLog,
                     [newRoomId]:  date
                 })
 
                 dispatch(updateAlertLog({
+                    transmissionId: data.transmissionID,
                     alertTitle: "User Added To Group",
                     alertTime: date,
                     alertInfo: {
@@ -117,7 +133,7 @@ export const socketIoListenForGlobalAlert = (dispatch, userId, userRooms, activi
             // so no updates
             // add to removed rooms list
             const roomId = data.eventData.roomId
-            if (data.eventData.addedUsers.includes(userId) && (userId !== data.eventData.updatedById)) {
+            if ( data.eventData.addedUsers.includes(userId) ) {
                 dispatch(socketIoLeaveRoom(roomId))
 
                 // update rooms, user, chatLog slices
@@ -127,26 +143,30 @@ export const socketIoListenForGlobalAlert = (dispatch, userId, userRooms, activi
 
                 // also rerender info for all users in the group
 
-                // add alert / notification
-                const date = Date.now()
-                dispatch(updateAlertLog({
-                    alertTitle: "User Removed From Group",
-                    alertTime: date,
-                    alertInfo: {
-                        roomName: data.eventData.roomName,
-                        updatedByUsername: data.eventData.updatedByUsername
-                    }
-                }))
+                if ( (userId !== data.eventData.updatedById) ) {
+                    // add alert / notification
+                    const date = Date.now()
+                    dispatch(updateAlertLog({
+                        transmissionId: data.transmissionID,
+                        alertTitle: "User Removed From Group",
+                        alertTime: date,
+                        alertInfo: {
+                            roomName: data.eventData.roomName,
+                            updatedByUsername: data.eventData.updatedByUsername
+                        }
+                    }))
+                }
             }
         }
 
         if (data.eventType === 'roomDeleted') {
+            console.log("roomdeleted")
             // socket io leave room
             // user rooms: removed from room, no longer listening (unjoined)
             // so no updates
             // add to deleted rooms list
             const roomId = data.eventData.roomId
-            if (userRooms?.includes(roomId) && userId !== data.eventData.deletedBy) {
+            if ( userRooms?.includes(roomId) ) {
                 dispatch(socketIoLeaveRoom(roomId))
 
                 dispatch(removeRoomFromUserSlice(roomId))
@@ -155,28 +175,32 @@ export const socketIoListenForGlobalAlert = (dispatch, userId, userRooms, activi
 
                 // update rooms, user, chatLog slices
 
-                // add alert / notification
-                const date = Date.now()
-                dispatch(updateAlertLog({
-                    alertTitle: "Group Deleted",
-                    alertTime: date,
-                    alertInfo: {
-                        roomName: data.eventData.roomName,
-                        deletedBy: data.eventData.deletedByUsername
-                    }
-                }))
+                if ( userId !== data.eventData.deletedBy ) {
+                    // add alert / notification
+                    const date = Date.now()
+                    dispatch(updateAlertLog({
+                        transmissionId: data.transmissionID,
+                        alertTitle: "Group Deleted",
+                        alertTime: date,
+                        alertInfo: {
+                            roomName: data.eventData.roomName,
+                            deletedBy: data.eventData.deletedByUsername
+                        }
+                    }))
+                }
             }
         }
 
         if (data.eventType === 'roomNameUpdated') {
             const roomId = data.eventData.roomId
-            if (userRooms?.includes(roomId) && userId !== data.eventData.updatedById) {
+            if (userRooms?.includes(roomId) && (userId !== data.eventData.updatedById)) {
                 // dispatch(socketIoLeaveRoom(roomId))
                 // update rooms, user, chatLog slices
 
                 // add alert / notification
                 const date = Date.now()
                 dispatch(updateAlertLog({
+                    transmissionId: data.transmissionID,
                     alertTitle: "Group Name Updated",
                     alertTime: date,
                     alertInfo: {
@@ -185,6 +209,11 @@ export const socketIoListenForGlobalAlert = (dispatch, userId, userRooms, activi
                         updatedByUsername: data.eventData.updatedByUsername,
                     }
                 }))
+
+                // room slice (+ chat log slice?) -> find index based on roomID, update roomName
+                const roomName = data.eventData.roomName
+                const transmissionID = data.transmissionID
+                dispatch(updateRoomNameReducer({roomId, roomName, transmissionID, transmissionIDs}))
 
             }
         }
